d24b54153a372bbf75329eb17cfd49952fc58529
André Lopes <datalopes1@gmail.com>
2024-07-08 19:14:04 -0300

    update: commit inicial

---
+++ scr/database.py
@@ -0,0 +1,21 @@
+#%% Import das bibliotevas
+import pandas as pd
+import sqlalchemy
+import os
+# %% Criação do database
+def csv_to_sqlite(db_name, directory):
+    engine = sqlalchemy.create_engine(f'sqlite:///{db_name}')
+    csv_files = [f for f in os.listdir(directory) if f.endswith('.csv')]
+
+    for csv_file in csv_files:
+        csv_path = os.path.join(directory, csv_file)
+        table_name = os.path.splitext(csv_file)[0]
+
+        df = pd.read_csv(csv_path, encoding = "iso-8859-1")
+        df.to_sql(table_name, engine, if_exists = 'replace', index = False)
+
+directory = '../data/raw'
+db_name = '../data/processed/database.db'
+#%% Criação do database
+csv_to_sqlite(db_name, directory)
+# %%
